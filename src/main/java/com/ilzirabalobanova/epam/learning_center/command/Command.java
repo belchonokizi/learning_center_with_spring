5ae95da85ddfa50d66d058bc5794e15d418c968a
belchonokizi <70578173+belchonokizi@users.noreply.github.com>
2022-02-24 11:28:02 +0400

    delete Command interface, Operation enum

--- src/main/java/com/ilzirabalobanova/epam/learning_center/command/Command.java
+++
@@ -1,8 +0,0 @@
-package com.ilzirabalobanova.epam.learning_center.command;
-
-import com.ilzirabalobanova.epam.learning_center.exception.*;
-
-public interface Command {
-
-    void execute() throws IllegalInitialDataException;
-}
