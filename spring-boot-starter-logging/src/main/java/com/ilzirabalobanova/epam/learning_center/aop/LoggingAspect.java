f9da5bad15ed0f88d7a071e5359af95aefcf1897
belchonokizi <70578173+belchonokizi@users.noreply.github.com>
2022-02-25 16:47:48 +0400

    create spring-boot-starter-logging

--- spring-boot-starter-logging/src/main/java/com/ilzirabalobanova/epam/learning_center/aop/LoggingAspect.java
+++ spring-boot-starter-logging/src/main/java/com/ilzirabalobanova/epam/learning_center/aop/LoggingAspect.java
@@ -1,21 +1,18 @@
 package com.ilzirabalobanova.epam.learning_center.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Component
+@Slf4j
 @Aspect
 public class LoggingAspect {
-    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
     @Pointcut(value = "execution(public void com.ilzirabalobanova.epam.learning_center.service.impl.StudentService.addStudent(*))")
     public void addMethodPointcut() {
@@ -24,7 +21,7 @@ public class LoggingAspect {
     @After("addMethodPointcut()")
     public void afterAddMethodAdvice(JoinPoint joinPoint) {
         List<Object> objects = Arrays.asList(joinPoint.getArgs());
-        objects.forEach(s -> logger.info("{} добавлен", s));
+        objects.forEach(s -> log.info("{} добавлен", s));
     }
 
     @Pointcut(value =
@@ -35,7 +32,7 @@ public class LoggingAspect {
     @AfterReturning(value = "deleteStudentMethodPointcut()", returning = "returnObject")
     public void afterDeleteStudentMethodAdvice(Object returnObject) {
         if (returnObject != null) {
-            logger.info("{} удален", returnObject);
+            log.info("{} удален", returnObject);
         }
     }
 
@@ -47,9 +44,9 @@ public class LoggingAspect {
     @AfterReturning(value = "sortAndShowStudentsMethodPointcut()", returning = "returnObject")
     public void afterSortAndShowStudentsMethodPointcutMethodAdvice(JoinPoint joinPoint, Object returnObject) {
         List<Object> args = Arrays.asList(joinPoint.getArgs());
-        args.forEach(arg -> logger.info("\nПараметр метода {}", arg));
+        args.forEach(arg -> log.info("\nПараметр метода {}", arg));
         if (returnObject != null) {
-            logger.info("\nВозвращаемый объект: \n{}", returnObject);
+            log.info("\nВозвращаемый объект: \n{}", returnObject);
         }
     }
 
