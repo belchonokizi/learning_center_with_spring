37524e6ae70ad3bc1d277c002b61e4a18e894285
belchonokizi <70578173+belchonokizi@users.noreply.github.com>
2022-03-14 16:53:09 +0400

    add queries for fill table students

---
+++ src/main/resources/queries/full_tables/const/full_students.sql
@@ -0,0 +1,29 @@
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Ivan", "Gorin", 1);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Anna", "Grushina", 2);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Elena", "Sovina", 3);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Oleg", "Kim", 1);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Igor", "Balin", 4);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Aleksandr", "Tutin", 5);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Marya", "Sidorova", 2);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Olga", "Nosova", 3);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Nikita", "Zorin", 4);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Maksim", "Boyko", 2);
---
+++ src/main/resources/queries/full_tables/test/full_students.sql
@@ -0,0 +1,14 @@
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Ivan", "Gorin", 1);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Anna", "Grushina", 2);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Elena", "Sovina", 3);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Igor", "Balin", 4);
+
+INSERT INTO students (first_name, last_name, program_id)
+VALUES ("Aleksandr", "Tutin", 5);
