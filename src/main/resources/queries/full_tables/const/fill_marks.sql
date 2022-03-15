4c8be84224a6b3a619639c558b8bfcc8f2996b38
belchonokizi <70578173+belchonokizi@users.noreply.github.com>
2022-03-15 11:29:21 +0400

    add queries for fill table marks

---
+++ src/main/resources/queries/full_tables/const/fill_marks.sql
@@ -0,0 +1,90 @@
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (1, 1, 80);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (1, 2, 70);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (1, 3, 90);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (2, 4, 65);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (2, 5, 85);
+
+INSERT INTO marks (student_id, module_id)
+VALUES (2, 6);
+
+INSERT INTO marks (student_id, module_id)
+VALUES (2, 7);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (3, 8, 60);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (3, 9, 100);
+
+INSERT INTO marks (student_id, module_id)
+VALUES (3, 10);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (4, 11, 50);
+
+INSERT INTO marks (student_id, module_id)
+VALUES (4, 12);
+
+INSERT INTO marks (student_id, module_id)
+VALUES (4, 3);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (5, 11, 70);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (5, 12, 90);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (6, 13, 50);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (6, 14, 60);
+
+INSERT INTO marks (student_id, module_id)
+VALUES (6, 15);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (7, 4, 80);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (7, 5, 70);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (7, 6, 80);
+
+INSERT INTO marks (student_id, module_id)
+VALUES (7, 7);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (8, 8, 100);
+
+INSERT INTO marks (student_id, module_id)
+VALUES (8, 10);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (9, 11, 90);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (9, 12, 90);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (10, 4, 80);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (10, 5, 70);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (10, 6, 10);
+
+INSERT INTO marks (student_id, module_id)
+VALUES (10, 7);
---
+++ src/main/resources/queries/full_tables/test/fill_marks.sql
@@ -0,0 +1,30 @@
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (1, 1, 80);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (1, 2, 70);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (2, 3, 65);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (2, 4, 85);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (3, 5, 60);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (3, 6, 100);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (4, 1, 50);
+
+INSERT INTO marks (student_id, module_id)
+VALUES (4, 2);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (5, 3, 70);
+
+INSERT INTO marks (student_id, module_id, value)
+VALUES (5, 4, 90);
