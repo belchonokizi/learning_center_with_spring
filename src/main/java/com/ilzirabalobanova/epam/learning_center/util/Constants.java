a0de2c89a58d21613b17225cf13cb5e47f0142b6
belchonokizi <70578173+belchonokizi@users.noreply.github.com>
2022-03-24 15:24:40 +0400

    refactoring

--- src/main/java/com/ilzirabalobanova/epam/learning_center/util/Constants.java
+++ src/main/java/com/ilzirabalobanova/epam/learning_center/util/Constants.java
@@ -1,8 +1,6 @@
 package com.ilzirabalobanova.epam.learning_center.util;
 
 public class Constants {
-    public static final String PATH_STUDENT_FILE = "src/main/resources/students.json";
-    public static final String PATH_YAML_FILE = "src/main/resources/properties.yaml";
     public static final String PATH_REPORT = "src/main/resources/report.txt";
     public static final int PASSING_SCORE = 75;
     public static final double HOURS_IN_DAY = 24.0;
